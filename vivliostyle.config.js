713f245de48a055e72bf673baccea69b4da0ff96
ogwata-book <90300942+ogwata-book@users.noreply.github.com>
2022-04-06 12:59:23 +0900

    Update vivliostyle.config.js

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: '私の本',
   author: '尾久綿次郎',
-  theme: 'css/style.css',
+  theme: 'https://github.com/vivliostyle/themes/tree/master/packages/%40vivliostyle/theme-academic',
   entry: [
     'chapter-2.md',
     'chapter-1.md',
