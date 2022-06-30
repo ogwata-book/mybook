19af89b55ac328be83eb64dbaeee83a9e5596445
ogwata-book <90300942+ogwata-book@users.noreply.github.com>
2022-07-01 00:14:51 +0900

    Update vivliostyle.config.js

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -4,8 +4,8 @@ module.exports = {
 //theme: 'css/style.css',
   entry: [
     //'Alice.md',
-    //'chapter-2.md',
-    'chapter-1.md',
+  'chapter-2.md',
+    //'chapter-1.md',
     //'chapter-3/section-2.md',
     //'fed.md'
   ]
